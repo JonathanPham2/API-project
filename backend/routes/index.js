f373c1a80f142928419202d6284b2c2a999d9c5c
Thanh Pham <jonathanphamw22@gmail.com>
2024-02-21 19:51:50 -0800

    Phase 1 complete

---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN",  req.csrfToken());
+    res.status(200).json({
+        "XSRF-TOKEN": csrfToken
+    })
+})
+
+module.exports = router
