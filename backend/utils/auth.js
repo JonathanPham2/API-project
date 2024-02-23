54cdf9bc37f25be0afcc8823d057656c221f3cbf
Thanh Pham <jonathanphamw22@gmail.com>
2024-02-23 01:29:06 -0800

    requireAuth-middlewrae

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -13,9 +13,40 @@ const setTokenCookie = (res, user) => {
         username: user.username,
     };
 
-    const token  = jwt.sign(safeUser, secret,{
+    const token  = jwt.sign({data: safeUser}, secret,{
         expiresIn: parseInt(expireIn)
     })
     
 
+};
+
+// 
+const restoreUser = (req, res, next ) => {
+    const { token } = req.body;
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayLoad) => {
+        if (err){
+            return next()
+        }
+        try {
+            const { id } = jwtPayLoad.data;
+            req.user = await User.findByPk(id, {
+                attributes: {
+                    include: ["email", "createdAt", "updatedAt"]
+                }
+            })
+             
+
+        }
+        catch(err) {
+            res.clearCookie(token);
+           return  next()
+
+        }
+        if(!req.user) res.clearCookie("token")
+        return next();
+    
+
+    })
 }
