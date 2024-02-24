a15769739d7d53b5787bbaff23543ad1ccf00bcc
Thanh Pham <jonathanphamw22@gmail.com>
2024-02-23 16:15:01 -0800

    Add a backend endpoint to get the current user session

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,6 +33,24 @@ router.post("/", async(req, res, next) => {
     })
 
 
+});
+
+router.get("/",(req, res) => {
+
+    // destructing user from request
+    const { user } = req;
+    // assuming restoreUser middleware alreay check..
+    if(user) {
+        const safeUser = {
+            id: user.id,
+            username: user.username,
+            email: user.email
+        }
+        return res.json({
+            user:safeUser
+        })
+    }
+    else return res.json({user: null})
 })
 
 
