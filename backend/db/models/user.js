a9b5cc5a7aaa5aac9ad00f719689f00402d76df2
Thanh Pham <jonathanphamw22@gmail.com>
2024-02-28 11:25:43 -0800

    finish all spot endpoints

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Spot, {
-        as: "Owner",
+        // as: "Owner",
         foreignKey: "ownerId",
         onDelete:"CASCADE",
         hooks: true
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const { Spot, Review, SpotImage, User} = require("../../db/models")
 const sequelize =  require("sequelize")
 const { requireAuth } = require("../../utils/auth");
+const { handleValidationErrors } = require("../../utils/validation");
+const { check } = require("express-validator");
+const spot = require("../../db/models/spot");
 
 
 
-// middlware that handling spots
 
 
 
@@ -87,14 +89,6 @@ return res.json({Spots: userSpots})
 
 
 });
-// // router.get("/test/:id", async(req,res,next)=> {
-// //     let id = req.params.id
-// //     let spot = await Spot.findByPk(id)
-// //     let img = await spot.getSpotImages({where:{
-// //         spotId: id
-// //     }})
-// //     res.json(img)
-// //  })
 
 router.get("/:spotId", async(req, res, next) => {
     const id =parseInt(req.params.spotId)
@@ -107,7 +101,7 @@ router.get("/:spotId", async(req, res, next) => {
                 },
                 {
                     model: SpotImage,
-                    attributes:["preview", "url"]
+                    attributes:["id","preview", "url"]
                 },
                 {
                     model: User,
@@ -139,12 +133,126 @@ router.get("/:spotId", async(req, res, next) => {
 
 
 })
+const validateSpot = [
+    check("address")
+    .exists({checkFalsy: true})
+    .withMessage("Stree address is required"),
+    check("city")
+    .exists({checkFalsy: true})
+    .withMessage("City is required"),
+    check("state")
+    .exists({checkFalsy: true})
+    .withMessage("state is required"),
+    check("country")
+    .exists({checkFalsy: true})
+    .withMessage("Country is required"),
+    check("lat")
+    .exists({checkFalsy: true})
+    .isFloat({ min: -90, max: 90})
+    .withMessage("Latitude must be within -90 and 90",),
+    check("lng")
+    .exists({checkFalsy: true})
+    .isFloat({ min: -180, max: 180})
+    .withMessage("Longitude must be withion -180 and 180"),
+    check("name")
+    .exists({checkFalsy: true})
+    .isLength({max: 50})
+    .withMessage("Name must be less than 50 characters"),
+    check("description")
+    .exists({checkFalsy: true})
+    .withMessage("Description is required"),
+    check("price")
+    .exists({checkFalsy: true})
+    .isNumeric()
+    .isFloat({min: 0.01})
+    .withMessage("Price per day must be a positive number"),
+    handleValidationErrors
+]
 
-// router.post("/", async (req, res, next )=> {
+router.post("/",requireAuth,validateSpot, async (req, res, next )=> { 
+    // retrieve all information in re.body
+    const { address, city, state, country, lat, lng, name ,description, price } = req.body;
+
+    const newSpot = await Spot.create({
+        ownerId: req.user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    })
+
+  return  res.status(201).json(newSpot)
 
 
-// })
+    
 
+
+})
+const validateOwnerSpot = async (req,res, next) => {
+    const spot = await Spot.findByPk(req.params.spotId)
+    if(spot && req.user.id === spot.ownerId){
+        req.spot = spot
+        return next()
+    }
+    else {
+        return res.status(400).json({
+            message:"Spot couldn't be found"
+        })
+    }
+}
+
+router.post("/:spotId/images", requireAuth,validateOwnerSpot, async(req, res, next) => {
+    const { url, preview } = req.body
+
+    const spot = req.spot;
+
+    const newImage = await spot.createSpotImage({
+        url,
+        preview
+    })
+    const newImageObj = newImage.toJSON()
+        delete newImageObj.createdAt
+        delete newImageObj.updatedAt
+        delete newImageObj.spotId
+        
+        
+        res.json(newImageObj);
+
+})
+router.put("/:spotId", requireAuth,validateOwnerSpot,validateSpot, async(req, res, next) => {
+    const { address, city, state, country, lat, lng, name ,description, price } = req.body;
+    const spotToUpdate = req.spot;
+
+    await spotToUpdate.update({
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+        
+    })
+    res.json(spotToUpdate)
+    
+ 
+})
+router.delete("/:spotId", requireAuth, validateOwnerSpot, async(req, res, next)=> {
+
+    const spotToDelete = req.spot;
+    spotToDelete.destroy();
+    res.json({
+        message: "Successfully delete"
+    })
+
+})
 
 
 module.exports = router
