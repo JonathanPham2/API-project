908801b49990b8940c47d927bb975309f34bbac9
Thanh Pham <jonathanphamw22@gmail.com>
2024-03-15 16:21:33 -0700

    fixed deployed issues

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -17,19 +17,19 @@ if(process.env.NODE_ENV === 'production'){
         )
     }
     )
+    // Serve the static assets in the frontend's build folder
+    router.use(express.static(path.resolve('../frontend/dist')));
+    
+    //Serve the frontend's index.html file at all other routes 
+    //Not starting with /api
+    router.get(/^(?!\/?api).*/, (req,res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+        )
+    });
 }
 
-// Serve the static assets in the frontend's build folder
-router.use(express.static(path.resolve('../frontend/dist')));
-
-//Serve the frontend's index.html file at all other routes 
-//Not starting with /api
-router.get(/^(?!\/?api).*/, (req,res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.sendFile(
-        path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
-    )
-});
 
 // Add a Xsrf- Token cookie in development
 if(process.env.NODE_ENV !== 'production'){
