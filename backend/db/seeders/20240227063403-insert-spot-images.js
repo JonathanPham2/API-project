870e7cf53036594f2e0eb0072812a8a2628cd075
Thanh Pham <jonathanphamw22@gmail.com>
2024-03-18 05:01:41 -0700

    added some images

--- backend/db/seeders/20240227063403-insert-spot-images.js
+++ backend/db/seeders/20240227063403-insert-spot-images.js
@@ -9,15 +9,15 @@ if(process.env.NODE_ENV === "production"){
 module.exports = {
   async up (queryInterface, Sequelize) {
   await SpotImage.bulkCreate([
-    {spotId: 1, url: "https://example.com/saigon1", preview: true},
+    {spotId: 1, url: "https://res.cloudinary.com/dzuhij5io/image/upload/v1710762109/luxury-unfurnished-villa-for-rent-in-ciputra-hanoi_20205161614_ikxsco.jpg", preview: true},
     {spotId: 1, url: "https://example.com/saigon2", preview: false},
-    {spotId: 2, url: "https://example.com/hanoi1", preview: true},
+    {spotId: 2, url: "https://res.cloudinary.com/dzuhij5io/image/upload/v1710763162/saigon_pqw9na.jpg", preview: true},
     {spotId: 2, url: "https://example.com/hanoi2", preview: false},
-    {spotId: 3, url: "https://res.cloudinary.com/dzuhij5io/image/upload/v1710762109/luxury-unfurnished-villa-for-rent-in-ciputra-hanoi_20205161614_ikxsco.jpg", preview: true},
+    {spotId: 3, url: "https://res.cloudinary.com/dzuhij5io/image/upload/v1710763163/sapa_k68fvp.avif", preview: true},
     {spotId: 3, url: "https://example.com/danang2", preview: false},
-    {spotId: 4, url: "https://example.com/hue1", preview: true},
+    {spotId: 4, url: "https://res.cloudinary.com/dzuhij5io/image/upload/v1710763161/nha_trang_padigz.jpg", preview: true},
     {spotId: 4, url: "https://example.com/hue2", preview: false},
-    {spotId: 5, url: "https://example.com/nhatrang1", preview: true},
+    {spotId: 5, url: "https://res.cloudinary.com/dzuhij5io/image/upload/v1710763161/hoii_an_wfp8pl.jpg", preview: true},
     {spotId: 5, url: "https://example.com/nhatrang2", preview: false}
   ],{validate:true})
   },
